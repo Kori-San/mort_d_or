4f0e4279b1b4f16060688bf3ad7131b102b21bc3
Kori-San <aymen@ezzayer.net>
2022-03-11 10:22:39 +0100

    Add Mort-d-or.py

---
+++ Mort-d-or.py
@@ -0,0 +1,8 @@
+class player :
+    def __init__(self, name, desc, type, hp, armor, weapon) :
+        self.name = name
+        self.desc = desc
+        self.type = type
+        self.hp = hp
+        self.armor = armor
+        self.weapon = weapon
