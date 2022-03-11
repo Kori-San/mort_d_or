cf582d303be4e341619277def4b5ae2237976a09
Tony Blard <tony.blard@epita.fr>
2022-03-11 11:31:15 +0100

    Ranger

--- Player/Ranger.py
+++ Player/Ranger.py
@@ -5,11 +5,3 @@ class Ranger(Player):
     def __init__(self, name, desc):
         super().__init__(name, desc, type = "Ranger",  hp= 60, armor =30, damage = 90)
         self.range = 90
-
-
-    def attack(self):
-       print(self.damage)
-
-    def talk(self):
-        print("Hello, I'm" + self.name + ", I'm a ranger, Here is what talk about me" + self.desc)
-
