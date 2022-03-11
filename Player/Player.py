533c14c6887f140fb76c1d5bdacdbd7cc290a18e
Tony Blard <tony.blard@epita.fr>
2022-03-11 10:43:37 +0100

    Add methods

--- Player/Player.py
+++ Player/Player.py
@@ -1,12 +1,11 @@
-
 class Player:
-    def __init__(self, name, desc, type, hp, armor, weapon):
+    def __init__(self, name, desc, type):
         self.name = name
         self.desc = desc
         self.type = type
-        self.hp = hp
-        self.armor = armor
-        self.weapon = weapon
 
     def attack(self):
-        pass;
+        pass
+
+    def talk(self):
+        pass
